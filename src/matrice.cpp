6883225a94164acb876f9912a23e60e4b4b3b791
acd <alex.donea92@gmail.com>
2017-04-27 19:36:40 +0300

    Update CameraVideo

--- src/matrice.cpp
+++ src/matrice.cpp
@@ -1,6 +1,31 @@
 #include <stdio.h>
+#include "opencv2\core\mat.hpp"
+#include <opencv2\opencv.hpp>
+using namespace cv;
+using namespace std;
+int main() {
+	//Mat a(2, 2, CV_32FC1, Scalar(1));
+	//Mat b(2, 2, CV_32FC1, Scalar(2));
+	//cout << a*b;
+	Mat a;
+	VideoCapture video(0);
+	if (!video.isOpened()) {
+		cout << "Cannot connect to camera";
+		getchar();
+		return 1;
+	}
+	//a = imread("C:\\Users\\Alex Donea\\Desktop\\ceas.jpg");
+	//namedWindow("imagine", WINDOW_NORMAL);
+	//imshow("imagine",a);
+	namedWindow("camera");
+	while (1) {
+		video >> a;
+		imshow("camera", a);
+		if (waitKey(1) == 27) {
+			break;
+		}
+	}
+	
 
-int main(void) {
-	printf("Salut");
-	return 0;
+	
 }
